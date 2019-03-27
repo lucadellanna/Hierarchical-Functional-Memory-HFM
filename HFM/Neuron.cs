dbfe3e4b3e348ba4cfc886e775f8e323da252939
lucadellanna <luca.d.dellanna@gmail.com>
2019-03-27 16:27:59 +0800

    Added logic for column inhibition

--- HFM/Neuron.cs
+++ HFM/Neuron.cs
@@ -15,6 +15,7 @@ namespace HFM
 		public List<Synapse> ProximalSynapses = new List<Synapse>();
 		public List<Synapse> BasalSynapses = new List<Synapse>();
 		public List<Synapse> ApicalSynapses = new List<Synapse>();
+		// Receptive fields are lists of neurons whose firing influences the firing of this neuron
 		public List<Neuron> ProximalReceptiveField => ProximalSynapses.Select(x => x.AxonFrom).ToList();
 		public List<Neuron> BasalReceptiveField => BasalSynapses.Select(x => x.AxonFrom).ToList();
 		public List<Neuron> ApicalReceptiveField => ApicalSynapses.Select(x => x.AxonFrom).ToList();
@@ -26,19 +27,19 @@ namespace HFM
 
 		public bool IsActive => ProximalSynapses.Count(x => x.IsActive) >=
 									   Constants.NUMBER_OF_PROXIMAL_SYNAPSES_TO_ACTIVATE
-					   && !GetIsInhibited();
+					   && !IsInhibited;
 
 		public bool IsPreactivated => BasalSynapses.Count(x => x.IsActive) >=
 									Constants.NUMBER_OF_BASAL_SYNAPSES_TO_ACTIVATE
 						|| ApicalSynapses.Count(x => x.IsActive) >=
 									Constants.NUMBER_OF_APICAL_SYNAPSES_TO_ACTIVATE;
 
-		public bool GetIsInhibited()
-		{
-			var neuronsInColumn = Brain.Neurons.Where(neuron => neuron.Coordinates.X == this.Coordinates.X &&
-													  neuron.Coordinates.Y == this.Coordinates.Y);
-			return neuronsInColumn.Any(neuron => neuron.IsPreactivated && !this.IsPreactivated);
-		}
+		public bool IsInhibitedByItsOwnColumn => Brain.Neurons.Where(neuron => neuron.Coordinates.X == this.Coordinates.X
+																	 && neuron.Coordinates.Y == this.Coordinates.Y)
+													  .Any(neuron => neuron.IsPreactivated && !this.IsPreactivated);
+
+		public bool IsInhibitedByOtherColumns => true;
+
+		public bool IsInhibited => IsInhibitedByItsOwnColumn || IsInhibitedByOtherColumns;
 	}
-}
 }
